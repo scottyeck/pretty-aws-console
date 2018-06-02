08b113fbc68e9666db8e766e06f8e1c9d7f1a58a
Scotty Eckenthal <scott.eckenthal@gmail.com>
2018-06-02 12:05:17 -0400

    Cleans up JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
-(function() {
-  function getIsCodeCommit(url) {
-    return url.split("/").indexOf("codecommit") > -1;
-  }
+(() => {
+  // Constants for each service
+  const services = {
+    codecommit: "codecommit"
+  };
 
-  if (getIsCodeCommit(window.location.href)) {
+  // URL tokens corresponding to each service
+  const urlTokens = {
+    [services.codecommit]: "codecommit"
+  };
+
+  /**
+   * Gets whether the provided URL is part of the specified AWS service.
+   *
+   * @param {String} service
+   * @param {String} url
+   * @returns {Boolean}
+   */
+  const getIsServiceUrl = (service, url = window.location) =>
+    url.split("/").indexOf(urlTokens[service]) > -1;
+
+  /**
+   * Gets the current service
+   *
+   * @returns {String}
+   */
+  const getCurrentService = () => Object.values(services).find(getIsServiceUrl);
+
+  /**
+   * Adds a classname to the body of the document that specifies
+   * which AWS service is currently in use.
+   *
+   * @param {String} service
+   */
+  const updateBodyClassNameByService = service => {
     document
-      .getElementsByTagName("body")[0]
-      .classList.add("pretty-aws-console-code-commit");
-  }
+      .getElementsByName("body")[0]
+      .classList.add(`pretty-aws-console-${service}`);
+  };
+
+  /**
+   * Gets the current service and updates the document accordingly.
+   */
+  const apply = () => {
+    updateBodyClassNameByService(getCurrentService());
+  };
+
+  apply();
 })();
