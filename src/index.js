f3a29a0f0191bc467e0e7f3e74313d6edef244b4
Scotty Eckenthal <scott.eckenthal@gmail.com>
2018-06-02 12:16:54 -0400

    Triage

--- src/index.js
+++ src/index.js
@@ -16,15 +16,17 @@
    * @param {String} url
    * @returns {Boolean}
    */
-  const getIsServiceUrl = (service, url = window.location) =>
+  const getIsServiceUrl = (service, url) =>
     url.split("/").indexOf(urlTokens[service]) > -1;
 
   /**
-   * Gets the current service
+   * Gets the current service based on the url
    *
+   * @param {String}
    * @returns {String}
    */
-  const getCurrentService = () => Object.values(services).find(getIsServiceUrl);
+  const getCurrentService = url =>
+    Object.values(services).find(service => getIsServiceUrl(service, url));
 
   /**
    * Adds a classname to the body of the document that specifies
@@ -34,7 +36,7 @@
    */
   const updateBodyClassNameByService = service => {
     document
-      .getElementsByName("body")[0]
+      .getElementsByTagName("body")[0]
       .classList.add(`pretty-aws-console-${service}`);
   };
 
@@ -42,7 +44,7 @@
    * Gets the current service and updates the document accordingly.
    */
   const apply = () => {
-    updateBodyClassNameByService(getCurrentService());
+    updateBodyClassNameByService(getCurrentService(window.location.href));
   };
 
   apply();
